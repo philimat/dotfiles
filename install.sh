b608d159d798c75f08197901a3ca11a40a2764e0
Matt Philippi <philippi.matthew@gmail.com>
2020-03-13 16:33:45 -0500

    refactored install and sync scripts

--- install.sh
+++ install.sh
@@ -1,31 +1,73 @@
 #!/bin/bash
+SHELL_CONFIGS=(".inputrc"
+)
+TMUX_CONFIG=".tmux.conf"
+GIT_CONFIG=".gitconfig"
+VIM_CONFIG=".vimrc"
+COC_CONFIG="coc-settings.json"
+
+echo 'OSTYPE is' $OSTYPE
+
 if [[ "$OSTYPE" == "linux"* ]]; then
     # Linux
-    echo 'Linux detected'
-    #echo 'copying dotfiles to ~'
-    cp -v -i ./{.gitconfig,.vimrc,.inputrc,.bashrc,.tmux.conf} ~/
-    #echo 'moving coc-settings to .vim directory for coc.nvim extension'
-    cp -v -i ./coc-settings.json ~/.vim
-    echo 'sourcing .inputrc, .bashrc and .tmux.conf'
-    source ~/{.inputrc,.bashrc,.tmux.conf}
-    echo 'Installing Vim Plugins...'
-    vim -E -c PlugClean -c qa
-    vim -E -c PlugInstall -c qa
-    echo 'Done!'
+    SHELL_CONFIGS+=(".bashrc")
+
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
-    echo 'Mac OS detected'
-    cp -v -i ./{.gitconfig,.vimrc,.inputrc,.bash_profile,.tmux.conf} ~/
-    echo 'moving coc-settings to .vim directory for coc.nvim extension'
-    cp -v -i ./coc-settings.json ~/.vim
-    echo 'sourcing .inputrc, .bash_profile and .tmux.conf'
-    source ~/{.inputrc,.bash_profile,.tmux.conf}
-    echo 'Installing Vim Plugins...'
-    vim -E -c PlugClean -c qa
-    vim -E -c PlugInstall -c qa
-    echo 'Done!'
+    SHELL_CONFIGS+=(".bash_profile")
 
 else
     # Unknown OS
-    echo "The operating system could not be detected. Config not installed."
+    echo  $OSTYPE 'not supported'
+    echo "dotfiles were not installed"
+    exit 1
 fi
+
+# overwrite shell configuration files
+for f in ${SHELL_CONFIGS[@]} $TMUX_CONFIG $GIT_CONFIG $VIM_CONFIG;
+do
+    cmp --silent ./$f ~/$f
+    exit_status=$?
+
+    if [[ $exit_status -eq 1 ]]; then
+       echo $f has updates
+       echo -n "would you like to see the diff? (y/n [n]) "
+       read -r ans
+       if [[ ans -eq 'y' ]]; then
+           git diff ./$f ~/$f
+       fi
+       cp -v -i ./$f ~/
+    fi
+
+done
+
+cmp --silent ./$COC_CONFIG ~/.vim/$COC_CONFIG
+exit_status=$?
+if [[ $exit_status -eq 1 ]]; then
+   echo $f has updates
+   echo -n "would you like to see the diff? (y/n [n]) "
+   read -r ans
+   if [[ ans -eq 'y' ]]; then
+       git diff ./$COC_CONFIG ~/.vim/$COC_CONFIG
+   fi
+   cp -v -i ./$COC_CONFIG ~/.vim
+fi
+
+# load shell configuration into current shell
+for f in ${SHELL_CONFIGS[@]};
+do
+    echo 'sourcing' $f
+    sleep .5
+    source ~/$f
+done
+
+# load tmux conf into current shell
+echo 'sourcing' $TMUX_CONFIG
+tmux source ~/$TMUX_CONFIG
+
+echo 'Installing Vim Plugins...'
+
+vim -E -c PlugClean -c qa
+vim -E -c PlugInstall -c qa
+
+echo 'Done!'
--- sync.sh
+++ sync.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 if [[ "$OSTYPE" == "linux"* ]]; then
     # Linux
-    echo 'Linux detected'
-    echo 'Syncing files...'
     rsync -v ~/{.gitconfig,.vimrc,.inputrc,.bashrc,.tmux.conf} ./
     rsync -v ~/.vim/coc-settings.json ./
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
-    echo 'Mac OS detected'
-    echo 'Syncing files...'
     rsync -v ~/{.gitconfig,.vimrc,.inputrc,.bash_profile,.tmux.conf} ./
     rsync -v ~/.vim/coc-settings.json ./
-
 else
     # Unknown OS
-    echo "The operating system could not be detected. Config files not synced."
+    echo $OSTYPE "not supported"
+    echo "Config files not synced."
+    exit 1
 fi

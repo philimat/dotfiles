92f0103a9c09bdc3062d4d01e882005483fbc95f
Matt Philippi <philippi.matthew@gmail.com>
2020-08-08 19:49:31 -0500

    updated Xcode install for betas

--- Xcode/install.sh
+++ Xcode/install.sh
@@ -1,8 +1,20 @@
 #/bin/bash
+XCODE_KEYBINDINGS_DIR=/Applications/Xcode.app/Contents/Frameworks/IDEKit.framework/Resources/
+XCODE_BETA_KEYBINDINGS_DIR=/Applications/Xcode-beta.app/Contents/Frameworks/IDEKit.framework/Resources/
+
 echo "Copying over Xcode UserData..."
 cp -ir ./UserData ~/Library/Developer/Xcode/
-echo "Copying over Xcode custom keybindings..."
-sudo cp -i ./IDETextKeyBindingSet.plist /Applications/Xcode.app/Contents/Frameworks/IDEKit.framework/Resources/
+
+if [[ -d "$XCODE_BETA_KEYBINDINGS_DIR" ]]; then
+    echo "Copying over Xcode custom keybindings..."
+    sudo cp -i ./IDETextKeyBindingSet.plist "$XCODE_KEYBINDINGS_DIR"
+fi
+
+if [[ -d "$XCODE_BETA_KEYBINDINGS_DIR" ]]; then
+    echo "Copying over Xcode beta custom keybindings..."
+    sudo cp -i ./IDETextKeyBindingSet.plist "$XCODE_BETA_KEYBINDINGS_DIR"
+fi
+
 echo "Copying over Xcode preferences..."
 cp -i ./com.apple.dt.Xcode.plist ~/Library/Preferences/
 echo 'Done!'

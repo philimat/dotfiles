0445df5a287587e3bb0c4cf29468a0529f2b14a9
Matt Philippi <philippi.matthew@gmail.com>
2020-12-22 11:50:04 -0600

    fixed bug in Xcode settings install.sh

--- Xcode/install.sh
+++ Xcode/install.sh
@@ -5,7 +5,7 @@ XCODE_BETA_KEYBINDINGS_DIR=/Applications/Xcode-beta.app/Contents/Frameworks/IDEK
 echo "Copying over Xcode UserData..."
 cp -ir ./UserData ~/Library/Developer/Xcode/
 
-if [[ -d "$XCODE_BETA_KEYBINDINGS_DIR" ]]; then
+if [[ -d "$XCODE_KEYBINDINGS_DIR" ]]; then
     echo "Copying over Xcode custom keybindings..."
     sudo cp -i ./IDETextKeyBindingSet.plist "$XCODE_KEYBINDINGS_DIR"
 fi
